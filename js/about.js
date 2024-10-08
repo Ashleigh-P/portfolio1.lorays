ff1aff2a3c65c3d40ae1706200228392199e0be5
Ashleigh P <ashleigh.e.phillips@gmail.com>
2024-10-08 19:04:01 +0100

    webpage updates

--- js/about.js
+++ js/about.js
@@ -20,4 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
       observer.observe(element);
     });
   });
+
+  document.querySelectorAll('.collapsible').forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+});
   
